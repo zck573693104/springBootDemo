88bb75169e7cd15563163576397896eb0a0ee065
zck <zck573693104@gmail.com>
2018-02-01 14:15:31 +0800

    提交缺失的返回值类

--- src/main/java/com/kcz/SpringBootDemoApplication.java
+++ src/main/java/com/kcz/SpringBootDemoApplication.java
@@ -1,9 +1,13 @@
 package com.kcz;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
-@org.springframework.boot.autoconfigure.SpringBootApplication
-public class SpringBootDemoApplication {
+@EnableScheduling
+@SpringBootApplication(exclude = {MongoAutoConfiguration.class,MongoDataAutoConfiguration.class})public class SpringBootDemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootDemoApplication.class, args);
